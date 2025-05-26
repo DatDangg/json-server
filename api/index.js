2c7abba4d928d3718fa8361841259f3d9adbb124
DatDangg <datdang20112003@gmail.com>
2025-05-26 22:58:07 +0700

    m

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(__dirname + "/../db.json"); // <-- chú ý đường dẫn
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use("/api", router);
 
+// Export handler theo đúng yêu cầu của Vercel
 module.exports = (req, res) => {
   server(req, res);
 };
